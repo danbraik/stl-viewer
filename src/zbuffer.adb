1132e67840b83b9d378a349a25130f65033aae9c
danbraik <danbraik@hotmail.com>
2013-10-13 14:00:45 +0200

    Clean

--- src/zbuffer.adb
+++ src/zbuffer.adb
@@ -8,9 +8,8 @@ package body ZBuffer is
 	buffer : array (Xmin..Xmax, Ymin..Ymax) of Float;
 
 
--- Draw a pixel on screen
+	-- Draw a pixel on screen
 	-- Can be called with an invalid position
-
 	procedure DrawPixel(X, Y : Integer ; V : PixLum) is
 	begin
 		if X >= Xmin and then X <= Xmax and then Y >= Ymin and then Y <= Ymax then
@@ -18,24 +17,26 @@ package body ZBuffer is
 		end if;
 	end;
 
+	-- Draw a pixel on screen (does not occlude nearest pixel)
+	-- Can be called with an invalid position
 	procedure DrawPixel(X, Y : Integer ; Z : Float ; V : PixLum) is
-		D : Float := Z;
 	begin
 		if X >= Xmin and then X <= Xmax and then Y >= Ymin and then Y <= Ymax then
-			D := Float'Rounding(D);
-			if buffer(X,Y) > D then
-				buffer(X, Y) := D;
+			-- Test if the current pixel is nearer than the older pixel
+			if buffer(X, Y) > Z then
+				-- Update the depth of the pixel
+				buffer(X, Y) := Z;
 				Dessin.Fixe_Pixel(X, Y, V);
 			end if;
 		end if;
 	end;
 
+	-- Reset pixels' depth
 	procedure Clear is
-
 	begin
 		for x in buffer'Range(1) loop
 			for y in buffer'Range(2) loop
-				buffer(x,y) := Float'Last;
+				buffer(x, y) := Float'Last;
 			end loop;
 		end loop;
 	end;
